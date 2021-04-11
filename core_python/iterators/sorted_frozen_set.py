efc6fb383f14b5e3a4ae5deb8a1c5fc3ad4679ea
Antoine Wood <antoinewood@gmail.com>
2021-04-11 13:52:25 -0400

    Constructor Tests Complete

--- core_python/iterators/sorted_frozen_set.py
+++ core_python/iterators/sorted_frozen_set.py
@@ -1,3 +1,12 @@
 class SortedFrozenSet:
     def __init__(self, items=None):
-        pass
+        self._items = (
+            set(items) if (items is not None)
+            else set()
+        )
+
+    def __contains__(self, item):
+        return item in self._items
+
+    def __len__(self):
+        return len(self._items)
