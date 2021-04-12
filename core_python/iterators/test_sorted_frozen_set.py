f132e59c2a3fd88f40e9f78782047824f822e294
antoine <antoine@thefurnitureguys.ca>
2021-04-11 23:54:12 -0400

    added getitem

--- core_python/iterators/test_sorted_frozen_set.py
+++ core_python/iterators/test_sorted_frozen_set.py
@@ -36,6 +36,7 @@ class TestContainerProtocol(unittest.TestCase):
     def test_negative_not_contained(self):
         self.assertFalse(9 not in self.s)
 
+
 class TestSizedProtocol(unittest.TestCase):
     def test_empty_with_default(self):
         s = SortedFrozenSet()
@@ -54,9 +55,87 @@ class TestSizedProtocol(unittest.TestCase):
         self.assertEqual(len(s), 10)
 
     def test_with_duplicates(self):
-        s = SortedFrozenSet([5,5,5])
+        s = SortedFrozenSet([5, 5, 5])
         self.assertEqual(len(s), 1)
 
+
+class TestIteratorProtocol(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.s = SortedFrozenSet([7, 2, 1, 1, 9])
+
+    def test_iter(self):
+        iterator = iter(self.s)
+        self.assertEqual(next(iterator), 1)
+        self.assertEqual(next(iterator), 2)
+        self.assertEqual(next(iterator), 7)
+        self.assertEqual(next(iterator), 9)
+        self.assertRaises(
+            StopIteration,
+            lambda: next(iterator)
+        )
+
+    def test_for_loop(self):
+        expected = [1, 2, 7, 9]
+        index = 0
+        for item in self.s:
+            self.assertEqual(item, expected[index])
+            index += 1
+
+
+class TestSequenceProtocol(unittest.TestCase):
+    def setUp(self) -> None:
+        self.s = SortedFrozenSet([1, 4, 9, 13, 15])
+
+    def test_index_zero(self):
+        self.assertEqual(self.s[0], 1)
+
+    def test_index_four(self):
+        self.assertEqual(self.s[4], 15)
+
+    def test_index_one_beyond_the_end(self):
+        with self.assertRaises(IndexError):
+            self.s[5]
+
+    def test_index_minus_one(self):
+        self.assertEqual(self.s[-1], 15)
+
+    def test_index_minus_five(self):
+        self.assertEqual(self.s[-5], 1)
+
+    def test_index_one_before_the_beginning(self):
+        with self.assertRaises(IndexError):
+            self.s[-6]
+
+    def test_slice_from_start(self):
+        self.assertEqual(self.s[:3], SortedFrozenSet([1, 4, 9]))
+
+    def test_slice_from_to_end(self):
+        self.assertEqual(self.s[3:], SortedFrozenSet([13, 15]))
+
+    def slice_empty(self):
+        self.assertEqual(self.s[10:], SortedFrozenSet())
+
+    def test_slice_arbitrary(self):
+        self.assertEqual(self.s[2:4], SortedFrozenSet([9, 13]))
+
+    def test_slice_step(self):
+        self.assertEqual(self.s[0:5:2], SortedFrozenSet([1, 9, 15]))
+
+    def test_slice_full(self):
+        self.assertEqual(self.s[:], self.s)
+
+
+class TestHashableProtocol(unittest.TestCase):
+
+    def test_equal_sets_have_same_hash_code(self):
+        self.assertEqual(
+            hash(SortedFrozenSet([5, 2, 1, 4])), hash(SortedFrozenSet([5, 2, 1, 4]))
+        )
+
+
+    def test_reversed(self):
+        s = SortedFrozenSet([])
 
 if __name__ == "__main__":
     unittest.main()
